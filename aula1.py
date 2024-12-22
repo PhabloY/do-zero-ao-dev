950bdf8da8df28f2a91020a6053e3cd258e8d534
Phablo Yuri <yuripw3p@outlook.com>
2024-12-22 09:52:43 -0300

    Update aula1.py

--- aula1.py
+++ aula1.py
@@ -1,1 +1,15 @@
+"""
+DocString
+E escrever o que eu
+quiser
 
+
+asdfasdfd
+"""
+
+''' Usar para escrever suas notas '''
+
+# Permite escrever um comentário
+print("welcome")  # Na frente
+# Abaixo
+print(456)
