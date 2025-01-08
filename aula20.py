e58563d0095878e451b19f69470f41300e386978
PhabloY <yuripw3p@outlook.com>
2025-01-08 20:34:51 -0300

    atualização

--- aula20.py
+++ aula20.py
@@ -1,3 +1,15 @@
-def generator(n=0):
-    yield 1
-    return 'ACABOU'
+# Introdução às Generator functions em Python
+# generator = (n for n in range(1000000))
+
+def generator(n=0, maximum=10):
+    while True:
+        yield n
+        n += 1
+
+        if n >= maximum:
+            return
+
+
+gen = generator(maximum=1000000)
+for n in gen:
+    print(n)
