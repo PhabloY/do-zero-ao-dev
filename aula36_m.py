adfcc9c5c92320d204149edd144b2b5ef15d9ee5
PhabloY <yuripw3p@outlook.com>
2025-01-10 00:54:08 -0300

    modulos e package

---
+++ aula36_m.py
@@ -0,0 +1,4 @@
+variavel_modulo = 'Phablo'
+
+def soma(x, y):
+    return x + y
---
+++ aula38_package/modulo.py
@@ -0,0 +1,14 @@
+# __all__ = [
+#     'variavel',
+#     'soma_do_modulo',
+# ]
+
+from aula38_package.modulo_b import fala_oi
+
+variavel = 'Alguma coisa'
+
+def soma_do_modulo(x, y):
+    return x + y
+
+nova_variavel = 'Ok'
+fala_oi()
