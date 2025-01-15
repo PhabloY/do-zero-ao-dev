46a7a9ba29667dd64f88c315805adb0617eb939c
PhabloY <yuripw3p@outlook.com>
2025-01-15 17:51:51 -0300

    modulo json

--- aula55.py
+++ aula55.py
@@ -1,25 +1,25 @@
 import json
 
-# pessoa = {
-#     'nome': 'Luiz Otávio 2',
-#     'sobrenome': 'Miranda',
-#     'enderecos': [
-#         {'rua': 'R1', 'numero': 32},
-#         {'rua': 'R2', 'numero': 55},
-#     ],
-#     'altura': 1.8,
-#     'numeros_preferidos': (2, 4, 6, 8, 10),
-#     'dev': True,
-#     'nada': None,
-# }
+pessoa = {
+    'nome': 'Luiz Otávio 2',
+    'sobrenome': 'Miranda',
+    'enderecos': [
+        {'rua': 'R1', 'numero': 32},
+        {'rua': 'R2', 'numero': 55},
+    ],
+    'altura': 1.8,
+    'numeros_preferidos': (2, 4, 6, 8, 10),
+    'dev': True,
+    'nada': None,
+}
 
-# with open('aula55.json', 'w', encoding='utf-8') as arquivo:
-#     json.dump(pessoa, 
-#               arquivo, 
-#               ensure_ascii=False,
-#               indent=2,
-#               )
+with open('aula55.json', 'w', encoding='utf-8') as arquivo:
+    json.dump(pessoa, 
+              arquivo, 
+              ensure_ascii=False,
+              indent=2,
+              )
 
-# with open('aula55.json', 'r', encoding='utf-8') as arquivo:
-#     pessoa = json.load(arquivo)
-#     print(pessoa['nome'])
+with open('aula55.json', 'r', encoding='utf-8') as arquivo:
+    pessoa = json.load(arquivo)
+    print(pessoa['nome'])
