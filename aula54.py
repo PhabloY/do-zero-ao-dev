9c4ae96bdcbf68a4b76a54c11d72b5f9eaf4a605
PhabloY <yuripw3p@outlook.com>
2025-01-15 15:45:56 -0300

    manipulação de arquivos

--- aula54.py
+++ aula54.py
@@ -1,3 +1,27 @@
+import os
+
+# Leia também: https://www.otaviomiranda.com.br/2020/normalizacao-unicode-em-python/
+# with open (context manager) e métodos úteis do TextIOWrapper
+# Usamos a função open para abrir
+# um arquivo em Python (ele pode ou não existir)
+# Modos:
+# r (leitura), w (escrita), x (para criação)
+# a (escreve ao final), b (binário)
+# t (modo texto), + (leitura e escrita)
+# Context manager - with (abre e fecha)
+# Métodos úteis
+# write, read (escrever e ler)
+# writelines (escrever várias linhas)
+# seek (move o cursor)
+# readline (ler linha)
+# readlines (ler linhas)
+# Vamos falar mais sobre o módulo os, mas:
+# os.remove ou unlink - apaga o arquivo
+# os.rename - troca o nome ou move o arquivo
+# Vamos falar mais sobre o módulo json, mas:
+# json.dump = Gera um arquivo json
+# json.load
+
 caminho_arquivo = 'C:\\Users\\user\\OneDrive\\Área de Trabalho\\teste\\'
 caminho_arquivo += 'aula54.txt'
 
@@ -5,25 +29,35 @@ caminho_arquivo += 'aula54.txt'
 
 # arquivo.close()
 
-with open(caminho_arquivo, 'w+') as arquivo:
-    arquivo.write('Linha 1\n')
+# with open(caminho_arquivo, 'w+') as arquivo:
+#     arquivo.write('Linha 1\n')
+#     arquivo.write('Linha 2\n')
+#     arquivo.writelines(
+#         ('Linha 3\n', 'linha 4\n')
+#     )
+#     arquivo.seek(0, 0)
+#     print(arquivo.read())
+#     print('Lendo')
+#     arquivo.seek(0, 0)
+#     print(arquivo.readline(), end='')
+#     print(arquivo.readline().strip())
+#     print(arquivo.readline().strip())
+#     print('Readlines')
+#     arquivo.seek(0, 0)
+#     for linha in arquivo.readlines():
+#         print(linha.strip())
+
+
+
+# with open(caminho_arquivo, 'r') as arquivo:
+#     print(arquivo.read())
+
+with open(caminho_arquivo, 'w', encoding='utf-8') as arquivo:
+    arquivo.write('Atenção\n')
     arquivo.write('Linha 2\n')
     arquivo.writelines(
         ('Linha 3\n', 'linha 4\n')
     )
-    arquivo.seek(0, 0)
-    print(arquivo.read())
-    print('Lendo')
-    arquivo.seek(0, 0)
-    print(arquivo.readline(), end='')
-    print(arquivo.readline().strip())
-    print(arquivo.readline().strip())
-    print('Readlines')
-    arquivo.seek(0, 0)
-    for linha in arquivo.readlines():
-        print(linha.strip())
 
-
-
-with open(caminho_arquivo, 'r') as arquivo:
-    print(arquivo.read())
+# os.unlink(caminho_arquivo)/remove faz a mesma coisa
+os.rename(caminho_arquivo, 'aula54_2.txt')
