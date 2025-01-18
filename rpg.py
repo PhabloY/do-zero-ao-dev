589e6411016eb7680110762351ca1e74b1c58cf4
PhabloY <yuripw3p@outlook.com>
2025-01-18 19:11:36 -0300

    funções completas

--- rpg.py
+++ rpg.py
@@ -5,9 +5,9 @@ lista_npcs = []
 
 player = {
     'nome': 'Phablo',
-    'level': '1',
+    'level': 1,
     'exp': 0,
-    'exp_max': 50,
+    'exp_max': 30,
     'hp': 100,
     'hp_max': 100,
     'dano': 25,
@@ -35,22 +35,35 @@ def exibir_npcs():
     for npc in lista_npcs:
         exibir_npc(npc)
 
+
 def exibir_npc(npc):
     print(
             f'nome: {npc['nome']} // level: {npc['level']} // dano: {npc['dano']} // HP: {npc['hp']} // EXP: {npc['exp']}'
             )
+
 
 def exibir_player():
     print(
             f'nome: {player['nome']} // level: {player['level']} // dano: {player['dano']} // HP: {player['hp']}/{player['hp_max']} // EXP: {player['exp']}/{player['exp_max']}'
             )
 
+
 def reset_player():
     player['hp'] = player['hp_max']
 
+
 def reset_npc(npc):
     npc['hp'] = npc['hp_max']
-    
+
+
+def level_up():
+    if player['exp'] >= player['exp_max']:
+        player['level'] += 1
+        player['exp'] = 0
+        player['exp_max'] = player['exp_max'] * 2
+        player['hp_max'] += 20
+        player['hp'] = player['hp_max']
+
 
 def iniciar_batalha(npc):
     while player['hp'] > 0 and npc['hp'] > 0:
@@ -65,7 +78,11 @@ def iniciar_batalha(npc):
     else:
         print(f'O {npc['nome']} venceu')
         exibir_npc(npc)
+
+    level_up()
+
     reset_player()    
+    reset_npc(npc)
 
 def atacar_npc(npc):
     npc['hp'] -= player['dano']
@@ -89,3 +106,8 @@ exibir_npcs()
 
 npc_selecionado = lista_npcs[0]
 iniciar_batalha(npc_selecionado)
+iniciar_batalha(npc_selecionado)
+iniciar_batalha(npc_selecionado)
+iniciar_batalha(npc_selecionado)
+iniciar_batalha(npc_selecionado)
+exibir_player()
