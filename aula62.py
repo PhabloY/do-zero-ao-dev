1ec5d0f07f213829f5ff1e3b7127beb5497ec51f
PhabloY <yuripw3p@outlook.com>
2025-01-19 20:56:03 -0300

    __dict__ e vars

--- aula62.py
+++ aula62.py
@@ -6,14 +6,21 @@ class Pessoa:
     def __init__(self, nome, idade):
         self.nome = nome 
         self.idade = idade
-        self.ano_atual = 2024
         
     def get_ano_nascimento(self):
         return Pessoa.ano_atual - self.idade
     
-p1 = Pessoa('João', 35)
-p2 = Pessoa('Phablo', 24)
 
-print(Pessoa.ano_atual)
-print(p1.get_ano_nascimento())
-print(p2.get_ano_nascimento())
+dados = {'nome': 'Phablo', 'idade': 35,}
+p1 = Pessoa(**dados)
+# p1.nome = 'Eita'
+# print(p1.nome)
+# p1.__dict__['outra'] = 'coisa'
+# p1.__dict__['nome'] = 'Phablo'
+# del p1.__dict__['nome']
+# print(p1.__dict__)
+# print(vars(p1))
+# print(p1.outra)
+# print(p1.nome)
+print(vars(p1))
+print(p1.nome)
