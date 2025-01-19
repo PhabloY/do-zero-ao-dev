eb49de3126f43cdff214ae71449a1c7026c6b8b5
PhabloY <yuripw3p@outlook.com>
2025-01-19 20:42:44 -0300

    atributos de classe

---
+++ aula62.py
@@ -0,0 +1,19 @@
+
+
+class Pessoa:
+    ano_atual = 2025
+
+    def __init__(self, nome, idade):
+        self.nome = nome 
+        self.idade = idade
+        self.ano_atual = 2024
+        
+    def get_ano_nascimento(self):
+        return Pessoa.ano_atual - self.idade
+    
+p1 = Pessoa('João', 35)
+p2 = Pessoa('Phablo', 24)
+
+print(Pessoa.ano_atual)
+print(p1.get_ano_nascimento())
+print(p2.get_ano_nascimento())
