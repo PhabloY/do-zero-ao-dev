9de2175b381cc18215b6ad84ddc66b8055afcd5d
PhabloY <yuripw3p@outlook.com>
2025-01-19 22:51:51 -0300

    solução alternativa

--- aula63_b.py
+++ aula63_b.py
@@ -1,23 +1,35 @@
 import json
 
-class Pessoa:
-    def __init__(self, nome, idade):
-        self.nome = nome
-        self.idade = idade
+from aula63_a import CAMINHO_ARQUIVO, Pessoa, salvar
 
-    def to_dict(self):
-        return {"nome": self.nome, "idade": self.idade, "cidade": self.cidade}
+# class Pessoa:
+#     def __init__(self, nome, idade):
+#         self.nome = nome
+#         self.idade = idade
 
-    @classmethod
-    def from_dict(cls, dados):
-        return cls(dados['nome'], dados['idade'])
+#     @classmethod
+#     def from_dict(self, dados):
+#         return self(dados['nome'], dados['idade'])
     
 
-with open('classe.json', 'r') as arquivo:
+# with open('classe.json', 'r') as arquivo:
+#     dados = json.load(arquivo)
+
+# pessoas = [Pessoa.from_dict(dado) for dado in dados]
+
+# for pessoa in pessoas:
+#     print(pessoa.nome, pessoa.idade)
+
+salvar()
+
+with open(CAMINHO_ARQUIVO, 'r') as arquivo:
     dados = json.load(arquivo)
+    p1 = Pessoa(**dados[0])
+    p2 = Pessoa(**dados[1])
+    p3 = Pessoa(**dados[2])
 
+    print(p1.nome, p1.idade)
+    print(p2.nome, p2.idade)
+    print(p3.nome, p3.idade)
 
-if isinstance(dados, dict):
-    pessoa = Pessoa.from_dict(dados)
-
-print(f"Nome: {pessoa.nome}, Idade: {pessoa.idade}")
+print(__name__)
