c853499d5eaee51ebe92f7cf31f70732c4e9477c
PhabloY <yuripw3p@outlook.com>
2025-01-22 02:47:50 -0300

    herança multipla

--- aula75.py
+++ aula75.py
@@ -14,26 +14,49 @@
 # string = MinhaString('Phablo')
 # print(string.upper())
 
-class A:
+class A(object):
     atributo_a = 'AAAAA'
+
+    def __init__(self, atributo):
+        self.atributo = atributo
 
     def metodo(self):
         print('A')
 
 class B(A):
     atributo_b = 'BBBBB'
+
+    def __init__(self, atributo, outra_coisa):
+        super().__init__(atributo)
+        self.outra_coisa = outra_coisa
+
     def metodo(self):
         print('B')
 
 class C(B):
     atributo_c = 'CCCCC'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # print('Olá')
+
     def metodo(self):
-        super().metodo()
+        # super().metodo()
+        # super(B, self).metodo()
+        # super(A, self).metodo()
+        A.metodo(self)
+        B.metodo(self)
         print('C')
 
 
-c = C()
-print(c.atributo_a)
-print(c.atributo_b)
-print(c.atributo_c)
+# print(C.mro())
+# print(B.mro())
+# print(A.mro())
+c = C('Atributo', 'Qualquer coisa')
+# print(c.atributo)
+# print(c.outra_coisa)
 c.metodo()
+# print(c.atributo_a)
+# print(c.atributo_b)
+# print(c.atributo_c)
+# c.metodo()
