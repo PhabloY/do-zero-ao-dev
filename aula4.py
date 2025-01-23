506b199a9b5904c228b1439085de63106904760a
PhabloY <yuripw3p@outlook.com>
2025-01-23 19:12:05 -0300

    conteudo do curso de python

--- aula4.py
+++
@@ -1,7 +0,0 @@
-nome = 'Phablo'
-altura = 1.70
-peso = 60
-imc = peso // altura ** 2
-print(nome,'tem', altura, 'de altura')
-print('pesa', peso, 'quilos e seu imc é')
-print(imc)
--- aula77/main.py
+++
@@ -1,9 +0,0 @@
-from eletronico import Smartphone
-
-galaxy_s = Smartphone('Galaxy S')
-iphone = Smartphone('Iphone')
-
-galaxy_s.ligar()
-iphone.desligar()
-
-
