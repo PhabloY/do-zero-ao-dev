48b6583f5ebe00add3a4125c60b9b1786c61c1fb
PhabloY <yuripw3p@outlook.com>
2025-01-30 19:30:45 -0300

    solução exercicio

---
+++ aula92/pessoas.py
@@ -0,0 +1,12 @@
+class Pessoa:
+    def __init__(self, nome: str, idade: int) -> None:
+        self.nome = nome
+        self.idade = idade
+
+    @property
+    def nome(self):
+        return self._nome
+    
+    @property
+    def idade(self):
+        return._idade
