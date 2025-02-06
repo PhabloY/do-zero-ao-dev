18efe14a74365ca8aa07ca0f5736aa0fc35be950
PhabloY <yuripw3p@outlook.com>
2025-02-06 17:40:40 -0300

    modulo os

--- aula103.py
+++ aula103.py
@@ -1,10 +1,30 @@
-# C: \Users\user\OneDrive\Área de Trabalho\curso_python\do-zero-ao-dev
-# os.walk para navegar de caminhos de forma recursiva
-# os.walk é uma função que permite percorrer uma estrutura de diretórios de
-# maneira recursiva. Ela gera uma sequência de tuplas, onde cada tupla possui
-# três elementos: o diretório atual (root), uma lista de subdiretórios (dirs)
-# e uma lista dos arquivos do diretório atual (files).
+# os + shutil - Apagando, copiando, movendo e renomeando pastas com Python
+# Vamos copiar arquivos de uma pasta para outra.
+# Copiar -> shutil.copy
+# Copiar Árvore recursivamente -> shutil.copytree
+# Apagar Árvore recursivamente -> shutil.rmtree
+# Apagar arquivos -> os.unlink
+# Renomear/Mover -> shutil.move ou os.rename
 import os
-
-caminho = os.path.join('/Users', 'user', 'OneDrive', 'Área de Trabalho',
-                       'curso_python', 'do-zero-ao-dev')
+import shutil
+HOME = os.path.expanduser('~')
+DESKTOP = os.path.join(HOME, 'Desktop')
+PASTA_ORIGINAL = os.path.join(DESKTOP, 'EXEMPLO')
+NOVA_PASTA = os.path.join(DESKTOP, 'NOVA_PASTA')
+shutil.rmtree(NOVA_PASTA, ignore_errors=True)
+shutil.copytree(PASTA_ORIGINAL, NOVA_PASTA)
+# shutil.move(NOVA_PASTA, NOVA_PASTA + '_EITA')
+shutil.rmtree(NOVA_PASTA, ignore_errors=True)
+# os.makedirs(NOVA_PASTA, exist_ok=True)
+# for root, dirs, files in os.walk(PASTA_ORIGINAL):
+#     for dir_ in dirs:
+#         caminnho_novo_diretorio = os.path.join(
+#             root.replace(PASTA_ORIGINAL, NOVA_PASTA), dir_
+#         )
+#         os.makedirs(caminnho_novo_diretorio, exist_ok=True)
+#     for file in files:
+#         caminho_arquivo = os.path.join(root, file)
+#         caminnho_novo_arquivo = os.path.join(
+#             root.replace(PASTA_ORIGINAL, NOVA_PASTA), file
+#         )
+#         shutil.copy(caminho_arquivo, caminnho_novo_arquivo)
