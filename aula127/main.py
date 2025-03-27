b927bb32a85cfae3fb5cfebd39e6436e7828f67e
PhabloY <yuripw3p@outlook.com>
2025-03-27 14:28:15 -0300

    sqlite3

--- aula127/main.py
+++ aula127/main.py
@@ -9,10 +9,15 @@ TABLE_NAME = 'customers'
 connection = sqlite3.connect(DB_FILE)
 cursor = connection.cursor()
 
+# CRUD - Create Read   Update Delete
+# SQL -  INSERT SELECT UPDATE DELETE
+
 # CUIDADO: fazendo delete sem where
 cursor.execute(
     f'DELETE FROM {TABLE_NAME}'
 )
+
+# DELETE mais cuidadoso
 cursor.execute(
     f'DELETE FROM sqlite_sequence WHERE name="{TABLE_NAME}"'
 )
@@ -51,6 +56,8 @@ cursor.executemany(sql, (
     {'nome': 'Joana', 'peso': 5},
 ))
 connection.commit()
-print(sql)
-
 cursor.close()
+connection.close()
+
+if __name__ == '__main__':
+    print(sql)
