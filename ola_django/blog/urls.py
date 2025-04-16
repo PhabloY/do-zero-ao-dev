9fd02c5a560e210626fe37fcf8c8e07726b251e0
PhabloY <yuripw3p@outlook.com>
2025-04-16 11:52:19 -0300

    urls estáticas e dinâmicas

--- ola_django/blog/urls.py
+++ ola_django/blog/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path
 from blog import views
+from django.urls import path
 
 app_name = 'blog'
 
 urlpatterns = [
     path('', views.blog, name='home'),
-    path('post/<int:id>', views.post, name='post'),
+    path('<int:id>/', views.post, name='post'),
     path('exemplo/', views.exemplo, name='exemplo'),
 ]
